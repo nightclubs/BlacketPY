2f9ec24ca7d2231743e356b8e443c981d70e37ec
zt <116030602+nightclubs@users.noreply.github.com>
2022-11-22 08:24:56 -0500

    added on_connect feature
    uses PHPSESSID to determine whether the account is eligible for the daily reward and returns the number of tokens in the account

--- BlacketPY.py
+++ BlacketPY.py
@@ -3,160 +3,231 @@ import threading
 import time
 import random
 
-legacy_url = "https://blacket.org/"
-session = requests.Session()
-
+legacy_url = 'https://blacket.org/'; session = requests.Session()
 
 class blacketErrors:
-    class InvalidLogin(Exception):
-        def __init__(self, message="Incorrect login please try again!"):
-            self.message = message
-            super().__init__(self.message)
+	class InvalidLogin(Exception):
+		def __init__(self, message="Incorrect login please try again!"):
+			self.message = message
+			super().__init__(self.message)
+		pass
 
-        pass
+	class FailedAuth(Exception):
+		def __init__(self, message="The server declined to connect."):
+			self.message = message
+			super().__init__(self.message)
+		pass
 
-    class FailedAuth(Exception):
-        def __init__(self, message="The server declined to connect."):
-            self.message = message
-            super().__init__(self.message)
+	class UnknownError(Exception):
+		def __init__(self, message="An unidentified error has occurred!"):
+			self.message = message
+			super().__init__(self.message)
+		pass
 
-        pass
-
-    class UnknownError(Exception):
-        def __init__(self, message="An unidentified error has occurred!"):
-            self.message = message
-            super().__init__(self.message)
-
-        pass
-
-    class BadRequest(Exception):
-        def __init__(self, message="Something went terribly wrong."):
-            self.message = message
-            super().__init__(self.message)
-
-        pass
-
+	class BadRequest(Exception):
+		def __init__(self, message="Something went terribly wrong."):
+			self.message = message
+			super().__init__(self.message)
+		pass
 
 class BlacketPY:
-    def login(user, password):
-        login_res = session.post(
-            legacy_url + "worker/user/login.php",
-            headers={
-                "authority": "blacket.org",
-                "accept": "*/*",
-                "accept-language": "en-US,en;q=0.9",
-                "cache-control": "no-cache",
-                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "origin": "https://blacket.org",
-                "pragma": "no-cache",
-                "referer": "https://blacket.org/market/",
-                "sec-ch-ua": '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": '"Windows"',
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-origin",
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42",
-                "x-requested-with": "XMLHttpRequest",
+    def on_connect(auth):
+     try:
+        connect_res = session.get(
+            legacy_url + 'worker/user/getusername.php',
+            cookies = {
+                'PHPSESSID': auth
             },
-            data={
-                "username": user,
-                "password": password,
+            headers = {
+                'authority': 'blacket.org',
+                'accept': '*/*',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'no-cache',
+                'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'origin': 'https://blacket.org',
+                'pragma': 'no-cache',
+                'referer': 'https://blacket.org/market/',
+                'sec-ch-ua': '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42',
+                'x-requested-with': 'XMLHttpRequest',
             },
         )
-        if "SUCCESS" in login_res.text:
-            return (
-                "successfully logged in | PHPSESSID: %s"
-                % login_res.cookies["PHPSESSID"]
-            )  # returns login cookie as well.
+        token_res = session.get(
+            legacy_url + 'worker/user/gettokens.php',
+            cookies = {
+                'PHPSESSID': auth
+            },
+            headers = {
+                'authority': 'blacket.org',
+                'accept': '*/*',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'no-cache',
+                'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'origin': 'https://blacket.org',
+                'pragma': 'no-cache',
+                'referer': 'https://blacket.org/market/',
+                'sec-ch-ua': '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42',
+                'x-requested-with': 'XMLHttpRequest',
+            },
+        )
+        reward_res = session.get(
+            legacy_url + 'worker/user/canclaimrewards.php',
+            cookies = {
+                'PHPSESSID': auth
+            },
+            headers = {
+                'authority': 'blacket.org',
+                'accept': '*/*',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'no-cache',
+                'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'origin': 'https://blacket.org',
+                'pragma': 'no-cache',
+                'referer': 'https://blacket.org/market/',
+                'sec-ch-ua': '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42',
+                'x-requested-with': 'XMLHttpRequest',
+            },
+        )
+        return '%s | %s | %s' % (connect_res.text, reward_res.text, token_res.text)
+        
 
+        
+     except Exception:
+        pass
+
+
+    def login(user, password):
+     try:
+        login_res = session.post(
+            legacy_url + 'worker/user/login.php',
+            headers = {
+                'authority': 'blacket.org',
+                'accept': '*/*',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'no-cache',
+                'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'origin': 'https://blacket.org',
+                'pragma': 'no-cache',
+                'referer': 'https://blacket.org/market/',
+                'sec-ch-ua': '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42',
+                'x-requested-with': 'XMLHttpRequest',
+            },
+            data = {
+                'username': user,
+                'password': password,
+            }
+        )
+        if 'SUCCESS' in login_res.text:
+            return 'successfully logged in | PHPSESSID: %s' % login_res.cookies['PHPSESSID'] # returns login cookie as well.
+            
         elif "Username and password don't match" in login_res.text:
             raise blacketErrors.InvalidLogin()
 
         else:
             raise blacketErrors.UnknownError()
-
+     except Exception:
+        pass
+            
     def buy_boxes(auth):
-        box_types = [
-            "Aquatic",
-            "Blizzard",
-            "Color",
-            "Dino",
-            "Ice Monster",
-            "Medieval",
-            "Safari",
-            "Space",
-            "Spooky",
-            "Wonderland",
-        ]
+     try:
+        box_types = ['Aquatic', 'Blizzard', 'Color', 'Dino', 'Ice Monster', 'Medieval', 'Safari', 'Space', 'Spooky', 'Wonderland']
         box = random.choice(box_types)
         box_res = session.post(
-            legacy_url + "worker/box/openbox.php",
-            cookies={"PHPSESSID": auth},  # your sess id.
-            headers={
-                "authority": "blacket.org",
-                "accept": "*/*",
-                "accept-language": "en-US,en;q=0.9",
-                "cache-control": "no-cache",
-                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "origin": "https://blacket.org",
-                "pragma": "no-cache",
-                "referer": "https://blacket.org/market/",
-                "sec-ch-ua": '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": '"Windows"',
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-origin",
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42",
-                "x-requested-with": "XMLHttpRequest",
+            legacy_url + 'worker/box/openbox.php',
+            cookies = {
+                'PHPSESSID': auth # your sess id.
             },
-            data={"box": box},
+            headers = {
+                'authority': 'blacket.org',
+                'accept': '*/*',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'no-cache',
+                'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'origin': 'https://blacket.org',
+                'pragma': 'no-cache',
+                'referer': 'https://blacket.org/market/',
+                'sec-ch-ua': '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42',
+                'x-requested-with': 'XMLHttpRequest',
+            },
+            data = {
+                'box': box
+            }
         )
         if box in box_res.text:
-            box_type = box_res.text.split("|")[0]
-            box_rarity = box_res.text.split("|")[1]
-            return "Successfully bought %s box and received %s (%s)!" % (
-                box,
-                box_type,
-                box_rarity,
-            )
+            box_type = box_res.text.split('|')[0]; box_rarity = box_res.text.split('|')[1]
+            return 'Successfully bought %s box and received %s (%s)!' % (box, box_type, box_rarity)
 
         elif "You're being rate limited.":
-            return "Rate Limited.."
+            return 'Rate Limited..'
 
         else:
             raise blacketErrors.UnknownError()
-
+     except Exception:
+        pass
+        
     def add_tokens(auth):
+     try:
         token_res = session.post(
-            legacy_url + "worker/box/openbox.php",
-            cookies={"PHPSESSID": auth},
-            headers={
-                "authority": "blacket.org",
-                "accept": "*/*",
-                "accept-language": "en-US,en;q=0.9",
-                "cache-control": "no-cache",
-                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "origin": "https://blacket.org",
-                "pragma": "no-cache",
-                "referer": "https://blacket.org/market/",
-                "sec-ch-ua": '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": '"Windows"',
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-origin",
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42",
-                "x-requested-with": "XMLHttpRequest",
+            legacy_url + 'worker/box/openbox.php',
+            cookies = {
+                'PHPSESSID': auth
             },
-            data="box=Add Tokens",
+            headers = {
+                'authority': 'blacket.org',
+                'accept': '*/*',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'no-cache',
+                'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'origin': 'https://blacket.org',
+                'pragma': 'no-cache',
+                'referer': 'https://blacket.org/market/',
+                'sec-ch-ua': '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42',
+                'x-requested-with': 'XMLHttpRequest',
+            },
+            data = 'box=Add Tokens'
         )
         if "You're" in token_res.text:
-            return "Please try again as the server is limited!"
+            return 'Please try again as the server is limited!'
 
-        elif "images" in token_res.text:
-            return "25k tokens were successfully added!"
+        elif 'images' in token_res.text:
+            return '25k tokens were successfully added!'
 
         else:
             raise blacketErrors.UnknownError()
+     except Exception:
+        pass
